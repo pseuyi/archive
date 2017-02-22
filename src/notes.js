4acc39f74e49377dd2b3c9c5a738df9d6f143915
freda nada <fredasnada@gmail.com>
2017-02-22 13:54:50 -0500

    add proj

--- src/notes.js
+++ src/notes.js
@@ -1,4 +1,12 @@
 export const notes = [
+
+  {
+    url: 'https://github.com/dyoungsmith/tabula-rasa',
+    name: 'tabula rasa',
+    type: 'vr app',
+    giturl: 'https://github.com/dyoungsmith/tabula-rasa',
+    desc: 'a vr app for remote whiteboarding build using aframe for google daydream/pixel. \n\ndeveloping this tool at fullstack academy for the 13-wk remote online course. students in the course learn all the skills needed to begin developing for the google daydream.'
+  },
   {
     url: 'https://drive.google.com/file/d/0By9AGPRY20L0cG1mQXJnSXNqcDQ/view',
     name: 'shared realities',
@@ -7,24 +15,18 @@ export const notes = [
     desc: 'an ongoing project with the digital story-telling lab @ columbia university to leverage ar/vr/mixed reality tools to create better dialogue between police officers and incarcerated youth in richmond, va. \n\ni was able to work with designers, social activists, and insightful teenagers to discuss early stage ideas for a vr training tool to improve policing. we discussed ux design challenges, whiteboarded narratives for empathy, and developed prototypes for tools that express the past, present, and future of incarcerated youth. \n\na spreadsheet of some of the ideas being developed can be found __[here.](https://docs.google.com/spreadsheets/d/1ZRLXVhjrNZg_iSO1HuL_p9iEAzOH4l0QXejgI9FYL5U/edit#gid=1337873918)__'
   },
   {
-    url: '/',
-    name: 'scalene',
-    giturl: 'https://github.com/pseuyi/scalene',
-    type: 'experimental web app',
-    desc: 'sound/space'
-  },
-  {
     url: '',
-    name: 'thermosyphon.io',
+    name: 'cyborg futures',
     giturl: '',
-    type: 'chat room',
-    desc: 'a chat application that is capable of tranferring heat to another user utilizing websockets and external infrared sensors. the idea for this came from walking by a windowless server farm exuding heat downtown in late winter.'
-  },
-  {
-    url: 'https://youtu.be/axkYGLtmlwc',
-    name: 'crypto consensus',
-    giturl: '',
-    type: 'presentation',
-    desc: 'a talk giving a brief overview of ethereum, smart contracts and javascript apps along the blockchain aka Dapps'
+    type: 'prototype',
+    desc: 'developing a new sense with a group of fabricators, artists, and ux designers for the cyborg foundation. we are working to conceptualize a way for humans to better understand radiation in a rapidly evolving environment where radiation, even at certain detectable wavelengths, goes largely unnoticed. \n\nwill this enhancement provide a new sense of danger when encountering ionizing radiation, for example? or will it augment our ability to feel warmth and connectedness. lab space provided by Parsons/The New School'
   }
 ]
+
+// {
+//   url: '',
+//   name: 'thermosyphon.io',
+//   giturl: '',
+//   type: 'chat room',
+//   desc: 'a chat application that is capable of tranferring heat to another user utilizing websockets and external infrared sensors. the idea for this came from walking by a windowless server farm exuding heat downtown in late winter.'
+// }
--- src/projects.js
+++ src/projects.js
@@ -24,6 +24,20 @@ export default [
     desc: 'sequence and process sampled music in a 3d environment. presentational mobile touch interface experimenting with nexusUI. \n\ncheck this project out in the [__tone.js demos__](https://tonejs.github.io/demos).'
   },
   {
+    url: 'https://github.com/pseuyi/post_human_weather',
+    name: 'post human weather',
+    giturl: 'https://github.com/pseuyi/post_human_weather',
+    type: 'wep app',
+    desc: 'post messages, emojis, and embedded media to an interactive globe created using webgl.'
+  },
+  {
+    url: 'https://vespertine-rhythms.herokuapp.com/',
+    name: 'vespertine rhythms',
+    giturl: 'https://github.com/pseuyi/vespertine_rhythms',
+    type: 'experimental web app',
+    desc: 'draw paths between various locations in nyc. location data comes from queries to the foursquare api.'
+  },
+  {
     url: 'https://stormy-forest-41556.herokuapp.com/products',
     name: 'carefull box',
     giturl: 'https://github.com/pseuyi/carefull_boxxes',
@@ -31,11 +45,25 @@ export default [
     desc: 'shop for gift boxes searchable by category. allows registered and unregistered users to keep track of their shopping cart and previous orders. \n\nmodeled with the popular online subscription box model in mind.'
   },
   {
-    url: 'https://github.com/pseuyi/post_human_weather',
-    name: 'post human weather',
-    giturl: 'https://github.com/pseuyi/post_human_weather',
-    type: 'wep app',
-    desc: 'post messages, emojis, and embedded media to an interactive globe created using webgl.'
+    url: 'https://cdn.rawgit.com/pseuyi/robitussyn/66188512/index.html',
+    name: 'robitussyn',
+    giturl: 'https://github.com/pseuyi/robitussyn',
+    type: 'vr experiment',
+    desc: 'my first app using aframe. this demo juxtaposes a tense scene from \'mulholland drive\' with a 3d rendering of the blue box prop. \n\nusing aframe click listeners to register interactivity.'
+  },
+  {
+    url: '/',
+    name: 'scalene',
+    giturl: 'https://github.com/pseuyi/scalene',
+    type: 'experimental web app',
+    desc: 'sound/space'
+  },
+  {
+    url: 'https://youtu.be/axkYGLtmlwc',
+    name: 'crypto consensus',
+    giturl: '',
+    type: 'presentation',
+    desc: 'a talk giving a brief overview of ethereum, smart contracts and javascript apps along the blockchain aka Dapps'
   },
   {
     url: 'https://github.com/pseuyi/_sSlimebase',
@@ -51,19 +79,5 @@ export default [
     giturl: 'https://github.com/pseuyi/xore.me',
     type: 'c.r.u.d. app',
     desc: 'save to and explore an anonymous library of music videos/playlists. \n\nan exercise to create persistent, userless data using a postgresSQL database and a clean, readable RESTful api'
-  },
-  {
-    url: 'https://vespertine-rhythms.herokuapp.com/',
-    name: 'vespertine rhythms',
-    giturl: 'https://github.com/pseuyi/vespertine_rhythms',
-    type: 'experimental web app',
-    desc: 'draw paths between various locations in nyc. location data comes from queries to the foursquare api.'
-  },
-  {
-    url: 'https://cdn.rawgit.com/pseuyi/robitussyn/66188512/index.html',
-    name: 'robitussyn',
-    giturl: 'https://github.com/pseuyi/robitussyn',
-    type: 'vr experiment',
-    desc: 'my first app using aframe. this demo juxtaposes a tense scene from \'mulholland drive\' with a 3d rendering of the blue box prop. \n\nusing aframe click listeners to register interactivity.'
   },
 ]
